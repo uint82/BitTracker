891d7c79b74763a5abbc32786cfebd0e99190274
uint82 <abrorhilmi6@gmail.com>
2025-09-07 14:20:28 +0700

    setup Vite + React + Tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">Hello World!</h1>
+    </div>
+  );
+};
+
+export default App;
